b928d8f01b08d7e4c1ba70bf6304c30b46f3eb06
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-02 11:36:23 -0700

    Start Psuedo code

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// View all Departments
+//SELECT * FROM departments
+
+// View all Roles
+// SELECT * FROM  roles
+
+// View all EMPLOYEE
+// SELECT * FROM  employee;
+
+// CREATE new department
+
+// prompt the user for the " name" of the department
+
+    // THEN run the query
+    // INSERT INTO department (name)
+    // VALUES  ("Sales");
+//       
+    // THEN as the user what they want to do next
+
+// create a new role
+
+//  get the existing department form the 'department' table
+    // THEN // Prompt the user for the "title", "salary" and department for the role
+
+        // Then Run the query
+        // INSERT INTO  role (title, salary, department_id)
+        // VALUES ("Manager", 120000, 1)
+
+            // THEN ask the user what they want to do next
