34655a8b69646f5d8324b1f081b160a33254ce1f
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-29 13:40:06 -0700

    Created Department table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
-DROP DATABASE IF EXISTS employee_trackerdb
-CREATE DATABASE 
+DROP DATABASE IF EXISTS employee_trackerdb;
+CREATE DATABASE employee_trackerdb;
+
+USE employee_trackerdb;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30) NOT Null
+);
